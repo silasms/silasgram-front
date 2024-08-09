08ca290ce35172a2c7cdb88654d59075c3c86f50
silasms <silas.medeiros7@gmail.com>
2024-08-09 20:06:19 -0300

    feat: logout in provider

--- src/contexts/auth.context.tsx
+++ src/contexts/auth.context.tsx
@@ -2,8 +2,10 @@ import { createContext, useState } from "react";
 
 export interface AuthContextValue {
   signedIn: boolean
-  user: object
+  user: { email: string, id: string, name: string }
   signin: (token: string, user?: object) => void
+  token: string
+  logout: () => void
 }
 
 export const AuthContext = createContext({} as AuthContextValue)
@@ -22,13 +24,28 @@ function getUser() {
   return user ? JSON.parse(user) : null
 }
 
+
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [ signedIn, setSignedIn ] = useState(isSigned())
   const [ user, setUser ] = useState(getUser())
+  const [ token, setToken ] = useState(getToken())
+
+  function getToken() {
+    const token = localStorage.getItem('TOKEN')
+    if (!token) return ""
+
+    return token
+  }
+
+  const logout = () => {
+    localStorage.clear()
+    setSignedIn(false)
+  }
 
   const signin = (token: string, user?: object) => {
     localStorage.setItem('TOKEN', token)
     setSignedIn(true)
+    setToken(token)
     
     if (!user) return
     localStorage.setItem('USER', JSON.stringify(user))
@@ -40,7 +57,9 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       value={{
         signedIn,
         user,
-        signin
+        signin,
+        token,
+        logout
       }}
     >
       {children}
